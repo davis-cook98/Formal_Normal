1e82ed1d030cb1e3acd9ef14837c18ded89740c8
Harshal Patel <44444186+hargadpatel@users.noreply.github.com>
2020-02-04 00:27:48 -0500

    Updated Formal Q1 -  Working with testing

--- Quiz1.rkt
+++ Quiz1.rkt
@@ -13,7 +13,7 @@
 
 
 (define (alg dfa)
-  (alg-helper dfa '(sm-getstart dfa) '(sm-getstart dfa)))
+  (alg-helper dfa (sm-getstart dfa) (sm-getstart dfa)))
 
 (define (alg-helper dfa state-list past-states)
   (letrec ([states-from-a (gets-to (car state-list))]
@@ -22,7 +22,6 @@
           [(null? state-list) (pretty-dfa dfa new-states)]
           [else (alg-helper dfa states-from-a new-states)])))
         
-  
 
 ;gets-to: a-state + rules -> states
 ;Purpose: To see what states we can get to from a state
@@ -126,14 +125,12 @@
               (Q5 b Q5))))
 
 
-; (check-expect (alg '((Q0 a Q1)
-;                                            (Q1 a Q0)
-;                                            (Q3 a Q3)
-;                                            (Q0 b Q0)
-;                                            (Q1 b Q1)
-;                                            (Q3 b Q3))) '((Q0 a Q1)(Q1 a Q0)(Q3 a Q3)(Q0 b Q0)(Q1 b Q1)(Q3 b Q3)))
-
-
+(check-expect (alg '((Q0 a Q1)
+                                           (Q1 a Q0)
+                                           (Q3 a Q3)
+                                           (Q0 b Q0)
+                                           (Q1 b Q1)
+                                           (Q3 b Q3))) '((Q0 a Q1) (Q1 a Q0) (Q3 a Q3) (Q0 b Q0) (Q1 b Q1) (Q3 b Q3)))
 
 (sm-testequiv? mach-1 mach-1-same)
 (sm-testequiv? mach-2 mach-2-same)
