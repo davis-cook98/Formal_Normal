2bc7360e74d353564b144a865d827d7230fe73c2
Davis <daviscook@outlook.com>
2020-02-04 01:45:28 -0500

    done?

--- Quiz1.rkt
+++ Quiz1.rkt
@@ -26,18 +26,19 @@
         [else (gets-to state-a (cdr rules))]))
 
 (define (been-there past-states new-states)
-  (if (eqv? 'Q0 new-states)
+  (if (symbol? new-states)
       (cond [(null? new-states) past-states]
-        [(member new-states past-states) past-states]
-        [else (been-there (cons new-states past-states))])
-  (cond [(null? new-states) past-states]
-        [(member (car new-states) past-states) (been-there past-states (cdr new-states))]
-        [else (been-there (cons (car new-states) past-states) (cdr new-states))])))
+            [(member new-states past-states) past-states]
+            [else (been-there (cons new-states past-states))])
+      (cond [(null? new-states) past-states]
+            [(member (car new-states) past-states) (been-there past-states (cdr new-states))]
+            [else (been-there (cons (car new-states) past-states) (cdr new-states))])))
 
 (define (remove-rules rules final-states)
-  (if (and (member (caddar rules) final-states)
-           (member (caar rules) final-states)) (cons (car rules) (remove-rules (cdr rules) final-states))
-                                               (remove-rules (cdr rules) final-states)))
+  (if (eqv? (caar rules) 'ds) '()
+      (if (and (member (cadr rules) final-states)
+               (member (caar rules) final-states)) (cons (car rules) (remove-rules (cdr rules) final-states))
+                                                   (remove-rules (cdr rules) final-states))))
 
 (define (pretty-dfa dfa reachable-states)
   (make-dfa
@@ -45,8 +46,8 @@
    (sm-getalphabet dfa)
    (sm-getstart dfa)
    (sm-getfinals dfa)
-   ;(remove-rules (sm-getrules dfa) reachable-states)))
-   (sm-getrules dfa)))
+   (remove-rules (sm-getrules dfa) reachable-states)))
+;(sm-getrules dfa)))
 
 
 (define mach-1
